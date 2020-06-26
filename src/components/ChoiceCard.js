6949f8d60b2b33582a79e31de13f37fd4cf9575f
Curt <curtis.larssen@gmail.com>
2020-06-26 18:48:01 +1200

    Revert "Small adjustments"
    This reverts commit b44fd53df347b65ef216b890b722202b8e36b0ae.

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -17,14 +17,10 @@ max-width:22em;
 @media (max-width: 700px) {
     margin:0.2em;
     min-width:31.25em;
-    padding:0.7em;
-    height:70px;
   }
   @media (max-width: 550px) {
     margin:0.2;
     min-width:21em;
-    height:60px;
-    padding:0.5em;
 }
 -webkit-touch-callout: none; /* iOS Safari */
     -webkit-user-select: none; /* Safari */
@@ -45,13 +41,6 @@ font-size:1.2rem;
 const AvatarImg = styled.img`
 border-radius:50%;
 max-height:48px;
-max-width:48px;
-@media (max-width: 700px) {
-    max-height:48px;
-max-width:48px;
-  }
-  @media (max-width: 550px) {
-    max-height:48px;
 max-width:48px;
 `
 
@@ -76,7 +65,7 @@ const ChoiceCard = ({ clicker, val, userChoice, ans }) => {
                 ?
                 (
                     <Card onClick={clicker}>
-                        < AvatarImg src={val[3]} alt={'avatar'} />
+                        < AvatarImg src={val[3]} alt={'avatar'}/>
                         <ChoiceBody>
                             <Name>{val[2]}</Name>
                             <Handle>{`@${val[1]} `}</Handle>
@@ -88,7 +77,7 @@ const ChoiceCard = ({ clicker, val, userChoice, ans }) => {
                     ?
                     (
                         <CorrectAnswer>
-                            < AvatarImg src={val[3]} alt={'avatar'} />
+                            < AvatarImg src={val[3]} alt={'avatar'}/>
                             <ChoiceBody>
                                 <Name>{val[2]}</Name>
                                 <Handle>{`@${val[1]} `}</Handle>
@@ -98,7 +87,7 @@ const ChoiceCard = ({ clicker, val, userChoice, ans }) => {
                     :
                     (
                         <WrongAnswer>
-                            < AvatarImg src={val[3]} alt={'avatar'} />
+                            < AvatarImg src={val[3]} alt={'avatar'}/>
                             <ChoiceBody>
                                 <Name>{val[2]}</Name>
                                 <Handle>{`@${val[1]} `}</Handle>
